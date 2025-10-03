f720dda12538d096371db599544cff23e8e621d7
BL4ZE <khushalverma3245@gmail.com>
2025-10-03 22:28:33 +0530

    Add files via upload

--- python assignmnet task 1.py
+++ python assignmnet task 1.py
@@ -2,13 +2,10 @@ Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (
 Enter "help" below or click "Help" above for more information.
 >>> 
 ========= RESTART: D:/Git hub connected files/.git/python assignment.py ========
->>> a=4
->>> b=2
->>> a+b
-6
->>> a-b
-2
->>> a*b
-8
->>> a/b
-2.0
+a = 2
+b = 4 
+a + b
+a - b 
+a * b 
+a / b
+
