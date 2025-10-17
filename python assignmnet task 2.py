b7e2fdb94443c949f2c36b86059987e0986bba4a
BL4ZE <khushalverma3245@gmail.com>
2025-10-17 22:43:24 +0530

    Update python assignmnet task 2.py

--- python assignmnet task 2.py
+++ python assignmnet task 2.py
@@ -1,6 +1,6 @@
 fname = input("enter your First Name : ")
 lname = input("enter your Last Name : ") 
-print("welcome to CAFE 'fname' + 'lname' ")
-
-
-
+msg = welcomme to cafe
+print(fname)
+print(lname)
+result = msg + fname + lname
